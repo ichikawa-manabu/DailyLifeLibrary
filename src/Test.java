d7751c1cc8e00312c0f5680be2e6bf34be76b72b
Manabu Ichikawa <manabu.ichikawa@ichilab.org>
2017-01-12 18:26:18 +0900

    2017.01.12 テストプログラムの更新
    テストプログラムを置いた。

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@
  * Created by manabu on 2017/01/12.
  */
 public class Test {
-    public static void main(String argsp[]) {
+    public static void main(String args[]) {
         System.out.println("Test");
     }
 }
