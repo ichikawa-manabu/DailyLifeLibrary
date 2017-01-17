43c89e8ff8d44b8e31b703963765cebb7686b865
Jiao2603 <xuejiao11@live.jp>
2017-01-18 01:09:07 +0900

    csvから算出可能　xlsからの算出方法作り中

--- src/Life_style/MAIN.java
+++ src/Life_style/MAIN.java
@@ -1,5 +1,6 @@
 package Life_style;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -12,8 +13,13 @@ import java.io.IOException;
 
 /**
  * Created by jiao on 2017/01/17.
+ * 作り中
  */
 public class MAIN {
+    //個体の長さ
+    public static int defaultGeneLength = 96;//24時間
+    //扱うパラメータの数 今回は10000人と想定する
+    public static final int NO_OF_PARAMETERS =10000;
     /*public static File file_in = new File("/Users/jiao/Desktop/国民生活時間調査/20代女性平日 ④時刻別行為者率（職業別、都市規模別ほか）.csv");
     public static File file_in2 = new File("/Users/jiao/Desktop/国民生活時間調査/20代女性②1日の行為者率・行為者平均時間量・全体平均時間量・標準偏差（国民全体、層別）.csv");
     //個体数
@@ -45,6 +51,12 @@ public class MAIN {
         int time;
         int page=2;
         time = sleep_period(file,page);
+//二つのファイル対応するページ番号が違います
+        int page2=3;
+        File file2 = new File("/Users/jiao/Desktop/国民生活時間調査/④時刻別行為者率（職業別、都市規模別ほか）.xls");
+        int[] standard_sleep_time = new int[defaultGeneLength];
+        standard_sleep_time=standard_sleep_time(file2,page);
+
     }
 
     //入力ファイル：②1日の行為者率・行為者平均時間量・全体平均時間量・標準偏差（国民全体、層別）.xls;
@@ -79,6 +91,111 @@ public class MAIN {
         }
 
          return time;
+
+        /*    for (int k = cellStart; k <= cellEnd; k++) {
+                HSSFCell cell = row.getCell(k);
+                if (null == cell) continue;
+                System.out.print("" + k + "  ");
+                //System.out.print("type:"+cell.getCellType());
+
+                switch (cell.getCellType()) {
+                    case HSSFCell.CELL_TYPE_NUMERIC: // 数字
+                        System.out.print(cell.getNumericCellValue()
+                                + "   ");
+                        break;
+                    case HSSFCell.CELL_TYPE_STRING: // String
+                        System.out.print(cell.getStringCellValue()
+                                + "   ");
+                        break;
+                    case HSSFCell.CELL_TYPE_BOOLEAN: // Boolean
+                        System.out.println(cell.getBooleanCellValue()
+                                + "   ");
+                        break;
+                    case HSSFCell.CELL_TYPE_FORMULA: // formula
+                        System.out.print(cell.getCellFormula() + "   ");
+                        break;
+                    case HSSFCell.CELL_TYPE_BLANK: // null
+                        System.out.println(" ");
+                        break;
+                    case HSSFCell.CELL_TYPE_ERROR: // dame
+                        System.out.println(" ");
+                        break;
+                    default:
+                        System.out.print(" Unavailable  ");//ダメ処理
+                        break;
+                }
+
+            }
+            //System.out.print("\n");*/
+
+    }
+
+    public static int[] standard_sleep_time(File file, int sheet_number) throws IOException {
+
+        POIFSFileSystem poifsFileSystem = new POIFSFileSystem(new FileInputStream(file));
+        HSSFWorkbook hssfWorkbook = new HSSFWorkbook(poifsFileSystem);
+        HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(sheet_number);
+
+        int[] time = new int[defaultGeneLength];
+        int rowstart = hssfSheet.getFirstRowNum();
+        int rowEnd = hssfSheet.getLastRowNum();
+        String cellValue = "";
+        for (int i = rowstart; i <= rowEnd; i++) {
+            //for (int i = rowstart; !cellValue.equals("睡　　　　　　 眠"); i++) {//睡眠の行を抽出する
+            HSSFRow row = hssfSheet.getRow(i);//第i行
+            if (null == row) continue;
+            int cellStart = row.getFirstCellNum();
+            int cellEnd = row.getLastCellNum();
+            HSSFCell cell = row.getCell(0);        //i行第1列
+           if(cell.getCellType()== HSSFCell.CELL_TYPE_BLANK){
+            //    cellValue = cell.getStringCellValue().trim();
+            }
+            //////
+
+          /*  switch (cell.getCellType()) {
+                case HSSFCell.CELL_TYPE_NUMERIC: // 数字
+                    System.out.print(cell.getNumericCellValue()
+                            + "   ");
+                    break;
+                case HSSFCell.CELL_TYPE_STRING: // String
+                    System.out.print(cell.getStringCellValue()
+                            + "   ");
+                    break;
+                case HSSFCell.CELL_TYPE_BOOLEAN: // Boolean
+                    System.out.println(cell.getBooleanCellValue()
+                            + "   ");
+                    break;
+                case HSSFCell.CELL_TYPE_FORMULA: // formula
+                    System.out.print(cell.getCellFormula() + "   ");
+                    break;
+                case HSSFCell.CELL_TYPE_BLANK: // null
+                    System.out.println(" ");
+                    break;
+                case HSSFCell.CELL_TYPE_ERROR: // dame
+                    System.out.println(" ");
+                    break;
+                default:
+                    System.out.print(" Unavailable  ");//ダメ処理
+                    break;
+            }*/
+            ////////
+
+            if(cellValue.equals("睡　　　　　　 眠")){
+                for (int k = 2; k < 2+defaultGeneLength; k++) {
+                    Cell cell_b = row.getCell(k);
+                    time[k - 2] = (int) (cell_b.getNumericCellValue() * NO_OF_PARAMETERS / 100);//
+                    System.out.println(time[k-2]);
+                }
+             //   Cell cell_b = row.getCell(3);        //睡眠平均時間を取る
+                // cellValue = cell_b.getStringCellValue().trim();
+               // double cellValue2 = cell_b.getNumericCellValue();//dateの形
+                //time=(int)(Math.floor(cellValue2*24)*4+Math.round((cellValue2*24-Math.floor(cellValue2*24))*60/15));
+                //System.out.println(time + "v"+i);
+                break;
+            }
+        }
+
+        return time;
 
         /*    for (int k = cellStart; k <= cellEnd; k++) {
                 HSSFCell cell = row.getCell(k);
